65b97433ceab29ce903b6d0c362c16b43adba3bb
Dev Nia <hello.yudana@gmail.com>
2024-07-15 16:14:35 +0700

    minify home.css

--- public/frontend/js/pages/home.js
+++ public/frontend/js/pages/home.js
@@ -1,43 +1,43 @@
- // Ambil elemen video
- const videoD = document.querySelector('.video-dekstop');
- const videoM = document.querySelector('.video-mobile');
- // Ambil elemen kontainer video
- const container = document.querySelector('.container_video_text');
+// Ambil elemen video
+const videoD = document.querySelector('.video-dekstop');
+const videoM = document.querySelector('.video-mobile');
+// Ambil elemen kontainer video
+const container = document.querySelector('.container_video_text');
 
- // Hitung tinggi kontainer video
- const containerHeight = container.offsetHeight;
+// Hitung tinggi kontainer video
+const containerHeight = container.offsetHeight;
 
- // Tentukan posisi tengah layar vertikal
- const middleOfScreen = window.innerHeight / 1;
+// Tentukan posisi tengah layar vertikal
+const middleOfScreen = window.innerHeight / 1;
 
- // Tentukan kapan scaling harus dimulai (misalnya pada 1999px dari atas kontainer)
- const startScalingAt = container.offsetTop + containerHeight - middleOfScreen;
+// Tentukan kapan scaling harus dimulai (misalnya pada 1999px dari atas kontainer)
+const startScalingAt = container.offsetTop + containerHeight - middleOfScreen;
 
- // Faktor penurunan kecepatan scaling
- const scaleSlowFactor = 0.5; // Sesuaikan nilai ini untuk kecepatan scaling yang diinginkan
+// Faktor penurunan kecepatan scaling
+const scaleSlowFactor = 0.5; // Sesuaikan nilai ini untuk kecepatan scaling yang diinginkan
 
- // Tambahkan event listener untuk mendeteksi scroll mouse
- window.addEventListener('scroll', function() {
-     // Dapatkan posisi scroll vertikal saat ini
-     let scrollPosition = window.scrollY + middleOfScreen;
+// Tambahkan event listener untuk mendeteksi scroll mouse
+window.addEventListener('scroll', function() {
+    // Dapatkan posisi scroll vertikal saat ini
+    let scrollPosition = window.scrollY + middleOfScreen;
 
-     // Hitung nilai scale berdasarkan posisi scroll
-     let scaleValue = 1;
-     if (scrollPosition >= startScalingAt) {
-         scaleValue = 1 - (scrollPosition - startScalingAt) * scaleSlowFactor / (containerHeight - middleOfScreen);
-         // Pastikan scaleValue tidak kurang dari 0
-         scaleValue = Math.max(scaleValue, 0);
-     }
+    // Hitung nilai scale berdasarkan posisi scroll
+    let scaleValue = 1;
+    if (scrollPosition >= startScalingAt) {
+        scaleValue = 1 - (scrollPosition - startScalingAt) * scaleSlowFactor / (containerHeight - middleOfScreen);
+        // Pastikan scaleValue tidak kurang dari 0
+        scaleValue = Math.max(scaleValue, 0);
+    }
 
-     // Terapkan transformasi pada video tanpa rotasi
-     videoD.style.transform = `scale(${scaleValue})`;
-     videoM.style.transform = `scale(${scaleValue})`;
- });
+    // Terapkan transformasi pada video tanpa rotasi
+    videoD.style.transform = `scale(${scaleValue})`;
+    videoM.style.transform = `scale(${scaleValue})`;
+});
+
+// end of video animation
 
 
-
- // Item Business
- document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     const elements = document.querySelectorAll('.item');
 
     function checkVisibility() {
@@ -58,19 +58,22 @@
     checkVisibility();
 });
 
-// VISION TEXT
- // Mendapatkan elemen vision_text
- const visionText = document.getElementById('vision_txt');
+// end of visibility animation
 
- // Mendeteksi saat pengguna melakukan scroll
- window.addEventListener('scroll', () => {
-     // Mendapatkan posisi scroll vertikal saat ini
-     const scrollPosition = window.scrollY;
+// Mendapatkan elemen vision_text
+const visionText = document.getElementById('visi_txt');
 
-     // Menentukan kondisi untuk mengubah skala
-     if (scrollPosition > 0) {
-         visionText.style.transform = 'scale(1)';
-     } else {
-         visionText.style.transform = 'scale(0.5)';
-     }
- });
+// Mendeteksi saat pengguna melakukan scroll
+window.addEventListener('scroll', () => {
+    // Mendapatkan posisi scroll vertikal saat ini
+    const scrollPosition = window.scrollY;
+
+    // Menentukan kondisi untuk mengubah skala
+    if (scrollPosition > 0) {
+        visionText.style.transform = 'scale(1)';
+    } else {
+        visionText.style.transform = 'scale(0.5)';
+    }
+});
+
+// end of vision text animation
